5eb75364074cc07f3c76dffce10b88fa6ed230ee
DolamoTrust <mtrustero@gmail.com>
2023-07-17 15:52:14 -0700

    Trust

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,12 @@
-#include <stdio.h>
+#include<stdio.h>
 /**
-*main - main entry point
-*return : always 0 (success)
+* main - Entry point
+* 
+* Return : Always 0 (success)
 */
 int main(void)
 {
 	printf("\"with proper grammar, but the outcome is a piece of art,");
 	return (0);
 }
+
